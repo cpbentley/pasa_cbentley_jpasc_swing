7c44e13cf87a623e6238acce15ef3b12a2d5650c
Charles Bentley <cbentley@mail.ru>
2019-10-01 17:36:30 +0200

    fix: prefs

--- src/pasa/cbentley/jpasc/swing/cmds/CmdTogglePrivacyCtx.java
+++ src/pasa/cbentley/jpasc/swing/cmds/CmdTogglePrivacyCtx.java
@@ -4,6 +4,7 @@ import pasa.cbentley.core.src4.event.BusEvent;
 import pasa.cbentley.core.src4.interfaces.ICommandable;
 import pasa.cbentley.jpasc.swing.ctx.IEventsPascalSwing;
 import pasa.cbentley.jpasc.swing.ctx.PascalSwingCtx;
+import pasa.cbentley.jpasc.swing.interfaces.IPrefsPascalSwing;
 import pasa.cbentley.swing.cmd.CmdSwingAbstract;
 
 /**
@@ -45,11 +46,17 @@ public class CmdTogglePrivacyCtx extends CmdSwingAbstract<ICommandable> implemen
 
    public void executeWith(ICommandable t) {
       boolean currentCtx = psc.isPrivateCtx();
-      psc.setPrivateCtx(!currentCtx);
+      boolean newValue = !currentCtx;
+      psc.setPrivateCtx(newValue);
+      
+      //save it in the settings for when the app starts again
+      psc.getPascPrefs().putBoolean(IPrefsPascalSwing.PREFS_PRIVATE_CTX, newValue);
+      
       BusEvent event = psc.getEventBusPascal().createEvent(PID_7_PRIVACY_CHANGES, EID_7_PRIVACY_1_CTX, this);
       psc.getEventBusPascal().putOnBus(event);
       psc.getSwingCtx().guiUpdateLater();
 
+      
       //TODO what about key names? we don't want to reveal them
       //custom keys have a private name and a public name
       //by default keys from wallet are private.. and are overriden in public context with a public name
